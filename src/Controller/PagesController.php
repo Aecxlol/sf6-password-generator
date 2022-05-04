20cc6d4b946f33e255d864f90e6de76a7c51ce59
unknown <aubry_damien@orange.fr>
2022-05-04 21:16:58 +0200

    password generation function

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -18,8 +18,51 @@ class PagesController extends AbstractController
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request): Response
     {
-        dump($request->query->all());
+        $password = '';
 
-        return $this->render('pages/password.html.twig');
+        $length                          = $request->query->getInt('length');
+        $uppercaseLetterOptionIsChecked  = $request->query->getBoolean('uppercase-letters');
+        $numberOptionIsChecked           = $request->query->getBoolean('numbers');
+        $specialCharacterOptionIsChecked = $request->query->getBoolean('special-characters');
+
+
+        $characters                 = range('a', 'z');
+        $lowercaseLettersArraySize  = count(range('a', 'z'));
+        $characters                 = array_merge($characters, range('A', 'Z'));
+        $uppercaseLettersArraySize  = count(range('A', 'Z'));
+        $characters                 = array_merge($characters, range(0, 9));
+        $numbersArraySize           = count(range(0, 9));
+        $specialCharacters          = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '|', '_', '^',
+            '?', '@', '[', ']', '<', '>'];
+        $characters                 = array_merge($characters, $specialCharacters);
+        $specialCharactersArraySize = count($specialCharacters);
+
+
+        while (strlen($password) < $length) {
+            $password = $password . $characters[mt_rand(0, $lowercaseLettersArraySize - 1)];
+
+            if ($uppercaseLetterOptionIsChecked) {
+                $password = $password . $characters[mt_rand($lowercaseLettersArraySize, $lowercaseLettersArraySize + $uppercaseLettersArraySize - 1)];
+            }
+
+            if ($numberOptionIsChecked) {
+                $password = $password . $characters[mt_rand($lowercaseLettersArraySize + $uppercaseLettersArraySize, $lowercaseLettersArraySize + $uppercaseLettersArraySize + $numbersArraySize - 1)];
+            }
+
+            if ($specialCharacterOptionIsChecked) {
+                $password = $password . $characters[mt_rand($lowercaseLettersArraySize + $uppercaseLettersArraySize + $numbersArraySize, $lowercaseLettersArraySize + $uppercaseLettersArraySize + $numbersArraySize + $specialCharactersArraySize - 1)];
+            }
+        }
+
+        if (strlen($password) > $length) {
+            $elementsToDelete = strlen($password) - $length;
+            $password         = substr($password, 0, -$elementsToDelete);
+        }
+
+        $password = str_shuffle($password);
+
+        return $this->render('pages/password.html.twig', [
+            'password' => $password
+        ]);
     }
 }
